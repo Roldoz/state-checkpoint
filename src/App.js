29e5ba26ab51a9f8f625165d44cd24c57cc115f9
Roldoz <rukfarfa@gmail.com>
2020-11-24 14:39:07 +0100

    state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+person = {
+  fullName : "Hatem Hamrouni",
+  bio : "Normal person, nothing special.",
+  imgSrc : "/pic.jpg",
+  profession :"Designer"
 }
+state = {
+  toggleShow : false,
+  time : 0
+}
+showHide = () => {
+  this.setState({
+    toggleShow : !this.state.toggleShow
+  })
+};
+componentDidMount() {
+  this.time = setInterval(()=>{
+    this.state.toggleShow
+    ? this.setState({time: this.state.time +1})
+  : this.setState({time : this.state.time})
+    },1000)
+}
+
+ render() {  
+   return (
+    <div className="App">
+      
+      <button onClick={this.showHide}>Show Or Hide</button>
+      {this.state.toggleShow ? 
+      <><h1>{this.person.fullName} </h1>
+      <h3>{this.person.bio} </h3>
+      <img src={this.person.imgSrc} width="250px"/>
+      <h2>{this.person.profession} </h2></> : <p>Click button to show</p>
+      }
+      <p>{this.state.time}</p>
+    </div>
+  )
+
+}}
 
 export default App;
